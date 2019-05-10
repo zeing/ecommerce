643ff5515d1545ed9017d5e0508911ef554ae94e
WWZ <zeing@outlook.com>
2019-05-10 21:09:39 +0700

    chore: add section 10/05/2019

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -26,31 +26,31 @@ class ProductItem extends React.Component {
   render() {
     const { name, description, image, price } = this.props
     return (
-      <Box
-        direction="column"
-        basis="medium"
-        pad="small"
-      >
-        <Box>
-          <Stack fill anchor="top-right">
-            <Box height="small">
-              <Image fit="cover" src={image} />
-            </Box>
-            <Box background="brand" pad="xsmall">
-              <Text>{price}</Text>
-            </Box>
-          </Stack>
+        <Box
+            direction="column"
+            basis="medium"
+            pad="small"
+        >
+          <Box>
+            <Stack fill anchor="top-right">
+              <Box height="small">
+                <Image fit="cover" src={image} />
+              </Box>
+              <Box background="brand" pad="xsmall">
+                <Text>{price}</Text>
+              </Box>
+            </Stack>
+          </Box>
+          <Box align="center">
+            <Heading textAlign="center" level={4} margin={{vertical: 'xsmall'}}>
+              {name}
+            </Heading>
+            <Text textAlign="center">
+              {description}
+            </Text>
+            <Button primary pad="small" margin="small" icon={<Shop />} label="Add to cart" onClick={this.handleAddToCart}/>
+          </Box>
         </Box>
-        <Box align="center">
-          <Heading textAlign="center" level={4} margin={{vertical: 'xsmall'}}>
-            {name}
-          </Heading>
-          <Text textAlign="center">
-            {description}
-          </Text>
-          <Button primary pad="small" margin="small" icon={<Shop />} label="Add to cart" onClick={this.handleAddToCart}/>
-        </Box>
-      </Box>
     )
   }
 }
